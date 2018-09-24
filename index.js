0aeaca9fc7100a11d621229e79dfe3b0c979cf73
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-09-24 22:42:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -512,17 +512,60 @@ client.on('guildMemberAdd', member => {
 
 
 
-
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("chat", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  }); 
+client.on('message', ALPHACODES => { 
+  var sender = ALPHACODES.author
+  if(!ALPHACODES.guild) return
+  if(!sw[ALPHACODES.guild.id]) sw[ALPHACODES.guild.id] = {
+  onoff: 'Off',
+  ch:    'Welcome',
+  msk:   'Welcome'
+}
+        if(ALPHACODES.content.startsWith(prefix + `set-wlc`)) {        
+        let perms = ALPHACODES.member.hasPermission(`MANAGE_CHANNELS`)
+        if(!perms) return ALPHACODES.channel.send('**You need `Manage Channels` permission**')
+        let args = ALPHACODES.content.split(" ").slice(1)
+        if(!args.join(" ")) return ALPHACODES.reply(`
+  ** ${prefix}set-wlc toggle **
+  ** ${prefix}set-wlc set [Channel Name] **
+  ** ${prefix}set-wlc msg [Welcome ALPHACODES] **`) // ->set-wlc toggle - ->set-wlc set - ->set-wlc msg
+        let state = args[0]
+        if(!state.trim().toLowerCase() == 'toggle' || !state.trim().toLowerCase() == 'set' || !state.trim().toLowerCase() == 'msg' ) return ALPHACODES.reply(`
+ ** ${prefix}set-wlc toggle **
+ ** ${prefix}set-wlc set [Channel Name] **
+ ** ${prefix}set-wlc msg [Welcome ALPHACODES] **`) // ->set-wlc toggle - ->set-wlc set - ->set-wlc msg
+        if(state.trim().toLowerCase() == 'toggle') { 
+        if(sw[ALPHACODES.guild.id].onoff === 'Off') return [ALPHACODES.channel.send(`**Welcome ALPHACODES Is **on** !**`), sw[ALPHACODES.guild.id].onoff = 'On']
+        if(sw[ALPHACODES.guild.id].onoff === 'On')  return [ALPHACODES.channel.send(`**Welcome ALPHACODES Is **off** !**`), sw[ALPHACODES.guild.id].onoff = 'Off']
+}
+        if(state.trim().toLowerCase() == 'set') {
+        let newch = ALPHACODES.content.split(" ").slice(2).join(" ")
+        if(!newch) return ALPHACODES.reply(`${prefix}set-wlc set [Channel name]`)
+        if(!ALPHACODES.guild.channels.find(`name`,newch)) return ALPHACODES.reply(`**I Cant Find This Channel.**`)
+            sw[ALPHACODES.guild.id].ch = newch
+            ALPHACODES.channel.send(`**Welcome channel Has Been Changed to ${newch}.**`)
+} 
+        if(state.trim().toLowerCase() == 'msg') {
+        let newmsg = ALPHACODES.content.split(" ").slice(2).join(" ")
+        if(!newmsg) return ALPHACODES.reply(`${prefix}set-wlc msg [New ALPHACODES]`)
+            sw[ALPHACODES.guild.id].msk = newmsg
+            ALPHACODES.channel.send(`**Welcome ALPHACODES Has Been Changed to ${newmsg}.**`)
+} 
+}
+        if(ALPHACODES.content === prefix + 'set-wlc info') {
+        let perms = ALPHACODES.member.hasPermission(`MANAGE_GUILD`) 
+        if(!perms) return ALPHACODES.reply(`You don't have permissions.`)
+        var embed = new Discord.RichEmbed()
+        .addField(`Welcome ALPHACODES  `, `
+On/Off  : __${sw[ALPHACODES.guild.id].onoff}__
+Channel : __${sw[ALPHACODES.guild.id].ch}__
+ALPHACODES : __${sw[ALPHACODES.guild.id].msk}__`)
+        .setColor(`BLUE`)
+        ALPHACODES.channel.send({embed})
+}
+        fs.writeFile("./setwlc.json", JSON.stringify(sw), (err) => {
+        if (err) console.error(err)
 });
-
+})
+//by ALPHA CODES 
 
 client.login(process.env.BOT_TOKEN);
