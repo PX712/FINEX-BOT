e1b4c6f9e90df17698992ee7c3aef4c69c37373b
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-09-26 19:08:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -215,7 +215,6 @@ client.on('message', message => {
 });
 /////
 
-const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
 const moment = require('moment');
