5086c714603dd6af84246b9fe3e76ec440b45700
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-09-26 17:12:38 +0300

    Update index.js

--- index.js
+++ index.js
@@ -195,17 +195,6 @@ client.on('message', message => {
 
 
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "welcome");
-     stewart.send(`<@${member.user.id}> تمت الدعوه من <@${inviter.id}>`);
-   //  stewart.send(`<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.`);
-  }); 
-});
-
 client.on('message', async function (message)  {
 if(message.content.startsWith(prefix+"server")) {
 const vlevel = ['None', 'Low (Must have verified email)', 'Medium (Must be register for 5 mineuts)', 'High (Need to wait 10 minutes)', 'Very High (Need verified phone on account)']
