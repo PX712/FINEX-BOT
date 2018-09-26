7aa08a74c9694fb6b7883ed7d6c4109a88eeb60a
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-09-26 18:19:29 +0300

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ if (message.content === prefix + "cmute") {
 
 
 
- client.on('message', message => {
+ client.on('message', message => {// برودكاست
 	    var prefix = "!";
               if(!message.channel.guild) return;
     if(message.content.startsWith(prefix + 'bc')) {
@@ -116,7 +116,7 @@ if (message.content === prefix + "cmute") {
     })
     }
     });
-});
+
 
 
 
@@ -146,13 +146,11 @@ client.on("ready", () => {
         })
     });
 });
-  
 
 
-  
 client.on('message', message => {
      if (message.author.bot) return;
-    if (message.content.startsWith("رابط")) {
+    if (message.content.startsWith("!رابط")) {
         message.channel.createInvite({
         thing: true,
         maxUses: 100,
@@ -162,7 +160,7 @@ client.on('message', message => {
     )
     const embed = new Discord.RichEmbed()
         .setColor("RANDOM")
-          .setDescription(" تم أرسال الرابط برسالة خاصة ")
+          .setDescription(" تم ارسال الرابط في الخاص :link: ")
            .setAuthor(client.user.username, client.user.avatarURL)
                  .setAuthor(client.user.username, client.user.avatarURL)
                 .setFooter('طلب بواسطة: ' + message.author.tag)
@@ -174,63 +172,6 @@ client.on('message', message => {
     .setDescription(" مدة الرابط : 3 ساعات  عدد استخدامات الرابط : 100 ")
       message.author.sendEmbed(Embed11)
     }
-});
-
-
-
-
-client.on('message', MEGA => { 
-  var sender = MEGA.author
-  if(!MEGA.guild) return
-  if(!sw[MEGA.guild.id]) sw[MEGA.guild.id] = {
-  onoff: 'Off',
-  ch:    'Welcome',
-  msk:   'Welcome'
-}
-        if(MEGA.content.startsWith(prefix + `set-wlc`)) {        
-        let perms = MEGA.member.hasPermission(`MANAGE_CHANNELS`)
-        if(!perms) return MEGA.channel.send('**You need `Manage Channels` permission**')
-        let args = MEGA.content.split(" ").slice(1)
-        if(!args.join(" ")) return MEGA.reply(`
-  ** ${prefix}set-wlc toggle **
-  ** ${prefix}set-wlc set [Channel Name] **
-  ** ${prefix}set-wlc msg [Welcome MEGA] **`) // ->set-wlc toggle - ->set-wlc set - ->set-wlc msg
-        let state = args[0]
-        if(!state.trim().toLowerCase() == 'toggle' || !state.trim().toLowerCase() == 'set' || !state.trim().toLowerCase() == 'msg' ) return MEGA.reply(`
- ** ${prefix}set-wlc toggle **
- ** ${prefix}set-wlc set [Channel Name] **
- ** ${prefix}set-wlc msg [Welcome MEGA] **`) // ->set-wlc toggle - ->set-wlc set - ->set-wlc msg
-        if(state.trim().toLowerCase() == 'toggle') { 
-        if(sw[MEGA.guild.id].onoff === 'Off') return [MEGA.channel.send(`**Welcome MEGA Is **on** !**`), sw[MEGA.guild.id].onoff = 'On']
-        if(sw[MEGA.guild.id].onoff === 'On')  return [MEGA.channel.send(`**Welcome MEGA Is **off** !**`), sw[MEGA.guild.id].onoff = 'Off']
-}
-        if(state.trim().toLowerCase() == 'set') {
-        let newch = MEGA.content.split(" ").slice(2).join(" ")
-        if(!newch) return MEGA.reply(`${prefix}set-wlc set [Channel name]`)
-        if(!MEGA.guild.channels.find(`name`,newch)) return MEGA.reply(`**I Cant Find This Channel.**`)
-            sw[MEGA.guild.id].ch = newch
-            MEGA.channel.send(`**Welcome channel Has Been Changed to ${newch}.**`)
-} 
-        if(state.trim().toLowerCase() == 'msg') {
-        let newmsg = MEGA.content.split(" ").slice(2).join(" ")
-        if(!newmsg) return MEGA.reply(`${prefix}set-wlc msg [New MEGA]`)
-            sw[MEGA.guild.id].msk = newmsg
-            MEGA.channel.send(`**Welcome MEGA Has Been Changed to ${newmsg}.**`)
-} 
-}
-        if(MEGA.content === prefix + 'set-wlc info') {
-        let perms = MEGA.member.hasPermission(`MANAGE_GUILD`) 
-        if(!perms) return MEGA.reply(`You don't have permissions.`)
-        var embed = new Discord.RichEmbed()
-        .addField(`Welcome MEGA  `, `
-On/Off  : __${sw[MEGA.guild.id].onoff}__
-Channel : __${sw[MEGA.guild.id].ch}__
-MEGA : __${sw[MEGA.guild.id].msk}__`)
-        .setColor(`BLUE`)
-            MEGA.channel.send({embed})
-}
-        fs.writeFile("./setwlc.json", JSON.stringify(sw), (err) => {
-        if (err) console.error(err)
 });
 
 
