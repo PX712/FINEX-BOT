c40ab2fa0cbb27b8c2a571356d30ec3fad030811
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-10-05 09:17:17 +0300

    Update index.js

--- index.js
+++ index.js
@@ -371,6 +371,37 @@ message.channel.send(args.join("  "))
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "『FINEX』")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            dat[Inv] = Invite.uses;
+        })
+    })
+})
 client.on("guildMemberAdd", (member) => {
     let channel = member.guild.channels.find('name', 'finex');
     if (!channel) {
