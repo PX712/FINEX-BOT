fd250d871cf2518752bda094eb50f6f79b1764e6
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-10-05 11:29:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -375,46 +375,30 @@ message.channel.send(args.join("  "))
 
 
 
-var dat = JSON.parse("{}");
-function forEachObject(obj, func) {
-    Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
-}
-client.on("ready", () => {
-    var guild;
-    while (!guild)
-        guild = client.guilds.find("name", "『FINEX』")
-    guild.fetchInvites().then((data) => {
-        data.forEach((Invite, key, map) => {
-            var Inv = Invite.code;
-            dat[Inv] = Invite.uses;
-        })
-    })
-})
 client.on("guildMemberAdd", (member) => {
-    let channel = member.guild.channels.find('name', 'finex');
+    let channel = member.guild.channels.get("423929653569257487");
     if (!channel) {
-        console.log("!channel fails");
+        console.log("!the channel id it's not correct");
         return;
     }
     if (member.id == client.user.id) {
         return;
     }
-    console.log('made it till here!');
+    console.log('-');
     var guild;
     while (!guild)
-        guild = client.guilds.find("name", "『FINEX』")
+        guild = client.guilds.get("423857340173910017");
     guild.fetchInvites().then((data) => {
         data.forEach((Invite, key, map) => {
             var Inv = Invite.code;
             if (dat[Inv])
                 if (dat[Inv] < Invite.uses) {
-                    console.log(3);
-                    console.log(${member} joined over ${Invite.inviter}'s invite ${Invite.code})
- channel.send(invited by : ${Invite.inviter} )            
+ channel.send(`invited by :  ${Invite.inviter} `) ;         
  }
             dat[Inv] = Invite.uses;
-        })
-    })
+       
+       });
+    });
 });
 
 
