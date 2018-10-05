aa9b9d08a272fac951f389fbcff49d66d782569e
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-10-05 09:59:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -376,10 +376,10 @@ message.channel.send(args.join("  "))
 
 
 
-
-
-const fs = require('fs');
+const Discord = require('discord.js');
+ const client = new Discord.Client()
 const jimp = require("jimp");
+const fs = require('fs');
 const Canvas = require('canvas');
  
 client.on('guildMemberAdd', member => {
