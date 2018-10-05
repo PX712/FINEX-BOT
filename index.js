41975c0b19c403be36494e4b51c6e1ed0959f460
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-10-05 09:12:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -371,21 +371,37 @@ message.channel.send(args.join("  "))
 
 
 
-
-client.on('guildMemberAdd', member => {
-    var embed = new Discord.RichEmbed()
-    .setAuthor(member.user.username, member.user.avatarURL)
-    .setThumbnail(member.user.avatarURL)
-    .setTitle(`عضو جديد`)
-    .setDescription(`اهلا بك في السيرفر`)
-    .addField(' :bust_in_silhouette:  انت رقم',`**[ ${member.guild.memberCount} ]**`,true)
-    .setColor('GREEN')
-    .setFooter('The King Bot', 'https://cdn.discordapp.com/icons/390551815072251904/418fa2788d8115808951c9881ba8f190.jpg')
-
-var channel =member.guild.channels.find('name', 'finex')
-if (!channel) return;
-channel.send({embed : embed});
+client.on("guildMemberAdd", (member) => {
+    let channel = member.guild.channels.find('name', 'finex');
+    if (!channel) {
+        console.log("!channel fails");
+        return;
+    }
+    if (member.id == client.user.id) {
+        return;
+    }
+    console.log('made it till here!');
+    var guild;
+    while (!guild)
+        guild = client.guilds.find("name", "finex ,")
+    guild.fetchInvites().then((data) => {
+        data.forEach((Invite, key, map) => {
+            var Inv = Invite.code;
+            if (dat[Inv])
+                if (dat[Inv] < Invite.uses) {
+                    console.log(3);
+                    console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+ channel.send(` ♥ **تم دعوته من قبل ${Invite.inviter} ♥ `)            
+ }
+            dat[Inv] = Invite.uses;
+        })
+    })
 });
+
+
+
+
+
 
 
 client.login(process.env.BOT_TOKEN);
