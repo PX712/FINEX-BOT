ebaa403b5bad0ef2b260f9b3d4f99bac2999ac79
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-10-10 07:25:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -303,35 +303,7 @@ if(msg.content.startsWith ('!server')) {
 }
 });
 //////////////////////////////////////////////////////////
-client.on('message', message => { 
-var prefix ="!";
-			 if (message.content.startsWith(prefix + "user")) {
- var args = message.content.split(" ").slice(1);
- let user = message.mentions.users.first();
- var men = message.mentions.users.first();
-		var heg;
-		if(men) {
-				heg = men
-		} else {
-				heg = message.author
-		}
-	var mentionned = message.mentions.members.first();
-		 var h;
-		if(mentionned) {
-				h = mentionned
-		} else {
-				h = message.member
-		}
-					 moment.locale('ar-TN');
-	var id = new  Discord.RichEmbed()
-	.setAuthor(message.author.username, message.author.avatarURL) 
-.setColor("#707070")
-.addField(': تاريخ دخولك للدسكورد', `${moment(heg.createdTimestamp).format('YYYY/M/D HH:mm:ss')} **\n** \`${moment(heg.createdTimestamp).fromNow()}\`` ,true) 
-.addField(': تاريخ دخولك لسيرفرنا  ', `${moment(h.joinedAt).format('YYYY/M/D HH:mm:ss')} \n \`${moment(h.joinedAt).fromNow()}\``, true)               
-.setFooter(`FX BOT`, 'https://is.gd/yPGeUe')                                 
-.setThumbnail(heg.avatarURL);
-message.channel.send(id)
-}       });
+
 ///////////////////////////////////////////////
 
 
