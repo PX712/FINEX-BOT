52e7f408a9863a1333a1967db324d5097f2eea6f
pythorxxx <43121272+pythorxxx@users.noreply.github.com>
2018-10-10 07:01:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -357,3 +357,67 @@ message.channel.send(args.join("  "))
 });
 
 
+
+
+
+
+
+
+var dat = JSON.parse("{}");
+function forEachObject(obj, func) {
+	Object.keys(obj).forEach(function (key) { func(key, obj[key]) })
+}
+client.on("ready", () => {
+	var guild;
+	while (!guild)
+			guild = client.guilds.find("name", "『FINEX』")
+	guild.fetchInvites().then((data) => {
+			data.forEach((Invite, key, map) => {
+					var Inv = Invite.code;
+					dat[Inv] = Invite.uses;
+			})
+	})
+})
+client.on("guildMemberAdd", (member) => {
+	let channel = member.guild.channels.find('name', 'finex');
+	if (!channel) {
+			console.log("!channel fails");
+			return;
+	}
+	if (member.id == client.user.id) {
+			return;
+	}
+	console.log('made it till here!');
+	var guild;
+	while (!guild)
+			guild = client.guilds.find("name", "『FINEX』")
+	guild.fetchInvites().then((data) => {
+			data.forEach((Invite, key, map) => {
+					var Inv = Invite.code;
+					if (dat[Inv])
+							if (dat[Inv] < Invite.uses) {
+									console.log(3);
+									console.log(`${member} joined over ${Invite.inviter}'s invite ${Invite.code}`)
+channel.send(`invited by : ${Invite.inviter}  `)            
+}
+					dat[Inv] = Invite.uses;
+			})
+	})
+});
+
+
+
+
+
+
+
+ client.on('message', message => { // Leaked by [ @Fr3on Gamer#9338 ]
+        var  user = message.mentions.users.first() || message.author;
+    if (message.content.startsWith(prefix + "avatar")) {
+message.channel.send(`This avatar For ${user} link : ${user.avatarURL}`);
+}
+});
+
+
+
+client.login(process.env.BOT_TOKEN);
